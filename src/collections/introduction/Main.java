25bd105304eb6269438900f44dce9c021de5afff
aasif-create <>
2026-07-09 23:30:25 +0530

    chore: initialization of java collections framework project

---
+++ src/collections/introduction/Main.java
@@ -0,0 +1,12 @@
+package collections.introduction;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        System.out.println(numbers);
+        numbers.remove(2);System.out.println(numbers);
+    }
+}
